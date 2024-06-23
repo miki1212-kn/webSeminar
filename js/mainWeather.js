07bd678ee6386960ea3835aac278303270407c8e
mk <kanzakimiki.12@gmail.com>
2024-06-23 23:33:39 +0900

    weatherApp start

---
+++ js/mainWeather.js
@@ -0,0 +1,8 @@
+// import "../scss/weatherApp.css";
+import { getWeather } from "./weather.js";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  (res) => {
+    console.log(res.data);
+  }
+);
---
+++ js/weather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+function getWeather(lat, lon, timezone) {
+  return axios.get(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+    {
+      param: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+      },
+    }
+  );
+}
